f5dad0e690fcec9a6bd59661cdb0e7a28e57ca6f
Melinda Mott <melinda.mott@gmail.com>
2023-05-11 14:02:37 -0700

    review this branch

--- functions/two_sum.py
+++ functions/two_sum.py
@@ -1,6 +1,2 @@
 def twoSum(n, t):
-    def twoSum(n, t, 4):
-    for i in range(len(n)):
-        for j in range(i + 1, len(n)):
-            if n[j] == t - n[i]:
-                return [i, j]
+   pass
